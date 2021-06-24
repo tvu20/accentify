6438d50ee9b176fb0dc391b44a5e3fdcf74d80a4
Giao Vu Dinh <tvudinh20@gmail.com>
2021-06-24 19:07:44 +0800

    settings

--- src/components/TestContent.js
+++ src/components/TestContent.js
@@ -5,7 +5,6 @@ const TestContent = () => {
   const accessToken = useSelector(state => state.auth.accessToken);
 
   const fetchData = useCallback(async () => {
-    console.log(accessToken);
     const response = await fetch(
       'https://api.spotify.com/v1/me/player/recently-played',
       {
@@ -21,8 +20,6 @@ const TestContent = () => {
   }, [accessToken]);
 
   useEffect(() => {
-    console.log('home screen');
-
     fetchData();
   }, [fetchData]);
 
