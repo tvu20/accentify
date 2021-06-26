e6b2e85e01223fbfc1370bb6fe887c54cf258f98
Giao Vu Dinh <tvudinh20@gmail.com>
2021-06-26 18:12:59 +0800

    refactoring

--- src/components/DataTest.js
+++ src/components/DataTest.js
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { fetchTrends, fetchRecs } from '../store/trend-actions';
@@ -13,32 +13,7 @@ const DataTest = () => {
   useEffect(() => {
     dispatch(fetchTrends(accessToken, topTracks));
     dispatch(fetchRecs(accessToken, topTracks));
-  }, [dispatch, accessToken, topTracks, fetchRecs]);
-
-  // const fetchRecs = useCallback(async () => {
-  //   const response = await fetch(
-  //     `https://api.spotify.com/v1/recommendations?seed_tracks=3xKsf9qdS1CyvXSMEid6g8%2C73kadnv7SduzhApfRUv5C0%2C0pIM7qcu5cOjqwFEFqbVn1%2C0hNOP5epEjX8Zj5aSGr6JU%2C2nMeu6UenVvwUktBCpLMK9,`,
-  //     {
-  //       headers: {
-  //         Authorization: 'Bearer ' + accessToken,
-  //       },
-  //     }
-  //   );
-  //   if (!response.ok) {
-  //     throw new Error('Fetching cart data failed.');
-  //   }
-  //   const data = await response.json();
-  //   return data;
-  // }, []);
-
-  // const testing = useCallback(async () => {
-  //   const result = await fetchRecs();
-  //   console.log('result: ', result);
-  // }, []);
-
-  // useEffect(() => {
-  //   testing();
-  // }, []);
+  }, [dispatch, accessToken, topTracks]);
 
   return <div>DATA TESTING</div>;
 };
